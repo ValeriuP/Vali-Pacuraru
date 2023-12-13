98aa14d5c7d7ca1b97f70dd708e895a8b7ff9c6d
ValeriuP <pacuraruvaleriu@gmail.com>
2023-12-13 18:31:27 +0200

    proiect

---
+++ Proiect HTML/javascript/index.js
@@ -0,0 +1,93 @@
+console.log("Hello")
+
+
+console.log("Text aicii!!!");
+console.log("Pacuraru Ghe. Valeriu");
+
+// alert("Alerta")
+document.getElementById("output").innerText="Am adaugat un text nou"
+
+// variabile
+
+// var  este prima variabila si foarte veche nu se mai foloseste
+
+// let  se foloseste este nou
+
+// const 
+
+// let output    output_1    variabila  isi poate sa isi schimbe valoare
+// let  output
+
+const  outPutconst=""
+// console.log(text)
+// let text;
+// text="asa se schimba valoarea"
+// console.log(text)
+
+// String sir de caractere
+
+// let text = "jfgjdkdkg"
+
+
+// Number   numar
+
+// let Number=100;
+// Boolean true/false   adevarat/fals
+// Boolean=false;
+
+// let a=10;
+// let b=3;
+// let c=a%b;
+// a=a+1;
+// a++;
+// a--
+// a=a+120;
+// a+=120
+// a-+120;
+// a*
+
+// console.log(a);
+// console.log(b);
+// console.log(a+b);
+// console.log(c);
+// console.log("text:+c")
+
+// console.log("text:"+c+a+b);
+// console.log(`Text : ${c} si ${a} si ${b}`);
+// console.log("text" +c+" si" +a+" si");
+// let test =typeof(9);
+// console.log(test)
+
+// for(let i=0;i<100;i++);
+// console.log(i);
+
+
+// let a=5;
+// a=a.toString()
+// let b= parseInt("4");
+// console.log(a+b);
+
+// let varsta=11;
+// varsta+=1;
+// let nume="pacuraru";
+// varsta=varsta*2;
+// console.log(varsta)
+// varsta*=2;
+// console.log(varsta)
+
+
+function printName()
+{
+    let userInput= document.getElementById("name_input").value;
+    console.log(userInput);
+
+
+}
+ let suma=sum(5,4)
+ console.log(suma)
+
+function sum(a,b)
+{
+return a+b;
+
+}
