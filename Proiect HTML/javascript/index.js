3ac4978601955363cc6d2bca1430e780904260cf
ValeriuP <pacuraruvaleriu@gmail.com>
2023-12-16 11:57:27 +0200

    cursjavascript

--- Proiect HTML/javascript/index.js
+++ Proiect HTML/javascript/index.js
@@ -1,11 +1,11 @@
-console.log("Hello")
+// console.log("Hello")
 
 
-console.log("Text aicii!!!");
-console.log("Pacuraru Ghe. Valeriu");
+// console.log("Text aicii!!!");
+// console.log("Pacuraru Ghe. Valeriu");
 
 // alert("Alerta")
-document.getElementById("output").innerText="Am adaugat un text nou"
+// document.getElementById("output").innerText="Am adaugat un text nou"
 
 // variabile
 
@@ -18,7 +18,7 @@ document.getElementById("output").innerText="Am adaugat un text nou"
 // let output    output_1    variabila  isi poate sa isi schimbe valoare
 // let  output
 
-const  outPutconst=""
+// const  outPutconst=""
 // console.log(text)
 // let text;
 // text="asa se schimba valoarea"
@@ -76,18 +76,137 @@ const  outPutconst=""
 // console.log(varsta)
 
 
-function printName()
+// function printName()
+// {
+//     let userInput= document.getElementById("name_input").value;
+//     console.log(userInput);
+
+
+// }
+//  let suma=sum(5,4)
+//  console.log(suma)
+
+// function sum(a,b)
+// {
+// return a+b;
+
+// }
+ 
+// Lectie 13.12.2023
+
+// trei variabile 
+// string
+//  numar
+//  numar
+
+// let test="George"
+// let a=10;
+// let b=20;
+
+// console.log(a*b);
+
+
+// let a=12;
+// let b=23;
+// console.log(a%2);
+// console.log(b%2);
+
+// console.log("Prima valoare este:"+a%2 + "a doua valoare este :" +b%2);
+
+// console.log(`Prima valoare este: ${a%2} si ${b%2}`)
+
+
+// console.log(`Text : ${c} si ${a} si ${b}`);
+
+// let momentan=document.getElementById("momentan")
+// momentan.innerText="am fost aici"
+
+// momentan.style.width="200px";
+// momentan.style.width="200px";
+// momentan.style.backgroundColor="grey";
+
+
+// let paragraf = document.getElementById("output");
+// console.log
+
+// let div2=document.getElementById("div2")
+// div2.style.width="200px";
+// div2.style.height="200px";
+// div2.style.backgroundColor="red"
+// div2.innerHTML=`<p id="p1" >Aici este textul </p> `;
+// Text.innerText = `Aici este textul 2`;
+
+// function printName(){
+//     console.log("ceva")
+//     console.log(sum)
+// }
+
+// function sum(a, b, c){
+//     return a + b + c ;
+// }
+
+function printName(){
+    let input_name = document.getElementById("name_input").value;
+    let input_email = document.getElementById("name_email").value;
+    let div1 =document.getElementById("div1");
+    let c = document.getElementById("bdhfghffj");
+    
+    if( c==null)
+    {
+        alert("nu am gasit div c");
+    }
+
+if( div1.style.width=="200px")
 {
-    let userInput= document.getElementById("name_input").value;
-    console.log(userInput);
-
-
+    div1.style.width="600px";
 }
- let suma=sum(5,4)
- console.log(suma)
 
-function sum(a,b)
+else
 {
-return a+b;
-
+    div1.style.width="200px"
 }
+
+if(input_name=="Tiberiu" || input_email=="Tiberiu"){
+    alert("Hello Tiberiu");
+}
+if(input_name!="Tiberiu"){
+    alert(" Hello other person");
+}
+}
+
+// let a=10;
+// if(a<5)
+// {
+//     console.log(`a este mai mic decat 5`);
+// }
+// console.log (a);
+// Operatori  Logici
+
+// Si  &&
+
+
+// else if(4==6){
+//     alert("Else");
+// }
+
+function clic()
+{
+    let momentan= document.getElementById("momentan")
+    let div1=document.getElementById("div1")
+    let div2=document.getElementById("div2")
+    if (div1.style.backgroundColor=="aqua")
+    {
+        console.log(div1.style.backgroundColor);
+        div1.innerText=div1.style.backgroundColor
+    }
+    if(div1.style.backgroundColor=="aqua" && momentan.style.backgroundColor=="brown" || div2.style.backgroundColor=="red"){
+
+div1.innerText=div1.style.backgroundColor
+    momentan.innerText=momentan.style.backgroundColor
+    div2.innerText=div2.style.backgroundColor
+
+    }
+    
+}
+
+
