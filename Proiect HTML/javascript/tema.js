1dfc8a0a395670f885e9903c628ce1ce22181c81
ValeriuP <pacuraruvaleriu@gmail.com>
2023-12-18 21:21:32 +0200

    practica

---
+++ Proiect HTML/javascript/tema.js
@@ -0,0 +1,42 @@
+// console.log("Click me")
+
+// let buton=document.getElementById("buton");
+// function showmyname()
+// {
+// console.log("Pacuraru Gheorghe Valeriu")
+// }
+
+// function clickme()
+
+// {
+// document.getElementById("Detali").innerText="Pacuraru"
+
+// }
+// function Vali()
+// {
+//     let a=parseInt (prompt ());
+//     let b=parseInt (prompt ());
+//     let c=parseInt (prompt ());
+
+// let p=document.getElementById("Detali");
+// if (a==b && b==c){
+//     p.innerText=a+b+c;
+//     p.style.color="blue";
+// }
+// else
+// {
+// p.innerText=a+b
+// }
+
+// }
+// function clic()
+// {
+//     let a=prompt()
+//     if (a%2==0)
+//     {
+//         document.getElementById("Btn").style.background="red"
+//     }
+//     else{
+//         document.getElementById("Btn").style.background="blue"
+//     }
+// }
