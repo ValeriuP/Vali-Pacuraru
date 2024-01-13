6fd8561a34c7cbcf9d804e0a2a0b6a3f769bf552
ValeriuP <pacuraruvaleriu@gmail.com>
2024-01-13 11:55:54 +0200

    Lecti

---
+++ Proiect HTML/javascript/recapitulare8,01,2024.js
@@ -0,0 +1,37 @@
+
+// var x=10;
+
+// console.log("2");
+// console.log("6");
+// let text = "test"
+// console.log(text[0]);
+
+// let color =["red", "green","blue"];
+// let numbers = [1,2,3,4,5,6,7,8,9,10];
+// let test=[2,3,4,56]
+// let all =color.concat(numbers);
+// let sentence =["Hello","","world"];
+// let text=sentence.join("*")
+// console.log(text)
+
+
+// let numerePare=[2,4,6,8,10];
+// for(let i=0; i<numerePare.length; i++) {
+//     numerePare[i]=numerePare[i]*2
+// console.log(numerePare [i])
+// }
+
+
+// let arr=[4,7,2];
+// arr.sort((a,b) => a-b);
+
+
+// arr.sort((a,b)=> b-a);
+// console.log(arr)
+
+let rezultate =[]
+for(let i=0; i<5 ;i++) { 
+    rezultate.push(prompt("55"))
+   
+}
+console.log(rezultate)
