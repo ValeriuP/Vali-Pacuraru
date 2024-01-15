375876c77a2616aa45fb772485baf98eb98123c7
ValeriuP <pacuraruvaleriu@gmail.com>
2024-01-15 18:54:58 +0200

    ceva

---
+++ Proiect HTML/javascript/index2.js
@@ -0,0 +1,21 @@
+console.log("Click me")
+let buton=document.getElementById("buton")
+function showmyname() 
+{
+console.log("Pacuraru Gheorghe Valeriu")
+}
+
+function clickme()
+{
+    alert("Alerta"+"Pacuraru")
+}
+
+function click()
+{
+
+document.getElementById("Detali").innerText="Pacuraru Valeriu"
+}
+
+
+   
+
--- Proiect HTML/javascript/tema.js
+++ Proiect HTML/javascript/tema.js
@@ -1,42 +1,43 @@
-// console.log("Click me")
+console.log("Click me")
 
-// let buton=document.getElementById("buton");
-// function showmyname()
-// {
-// console.log("Pacuraru Gheorghe Valeriu")
-// }
+let buton=document.getElementById("buton");
+function showmyname()
+{
+console.log("Pacuraru Gheorghe Valeriu")
+}
 
-// function clickme()
+function clickme()
 
-// {
-// document.getElementById("Detali").innerText="Pacuraru"
+{
+document.getElementById("Detali").innerText="Pacuraru"
 
-// }
-// function Vali()
-// {
-//     let a=parseInt (prompt ());
-//     let b=parseInt (prompt ());
-//     let c=parseInt (prompt ());
+}
+function Vali()
 
-// let p=document.getElementById("Detali");
-// if (a==b && b==c){
-//     p.innerText=a+b+c;
-//     p.style.color="blue";
-// }
-// else
-// {
-// p.innerText=a+b
-// }
+{
+    let a=parseInt (prompt ());
+    let b=parseInt (prompt ());
+    let c=parseInt (prompt ());
 
-// }
-// function clic()
-// {
-//     let a=prompt()
-//     if (a%2==0)
-//     {
-//         document.getElementById("Btn").style.background="red"
-//     }
-//     else{
-//         document.getElementById("Btn").style.background="blue"
-//     }
-// }
+let p=document.getElementById("Detali");
+if (a==b && b==c){
+    p.innerText=a+b+c;
+    p.style.color="blue";
+}
+else
+{
+p.innerText=a+b
+}
+
+}
+function clic()
+{
+    let a=prompt()
+    if (a%2==0)
+    {
+        document.getElementById("Btn").style.background="red"
+    }
+    else{
+        document.getElementById("Btn").style.background="blue"
+    }
+}
