5b68e4acff0eb8c25bd71cc2ff5ec3d491bf561e
ValeriuP <pacuraruvaleriu@gmail.com>
2024-01-15 11:25:28 +0200

    Lucru

---
+++ Proiect HTML/proiect/Practica javascript/practica13.01.2024.js
@@ -0,0 +1,26 @@
+// function logicalCalc(array, op ) {
+//     if (op=="AND"){
+//         return array.every((el)=>{
+//             return Boolean(el)});
+
+//     }
+//     if (op=="OR"){
+//         return array.some((el)=>{
+//             return Boolean(el)
+//         })
+//     }
+//      if (op=="XOR"){
+//         return array.filter((el)=>{
+//             return Boolean(el)}).length % 2===1
+
+//     }
+
+// }
+
+// function sorter(textbooks) {
+//     return textbooks.sort(function(a,b) {
+//         return a.tot.owerCase().localeCompare( b.tolowerCase());
+//     });
+// }
+
+
